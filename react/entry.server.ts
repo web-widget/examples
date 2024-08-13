7a5db3a5e699f56941c01bf09b23afecb17c1ecd
糖饼 <sugarpie.tang@gmail.com>
2024-08-13 21:37:17 +0800

    refactor

--- react/entry.server.ts
+++ react/entry.server.ts
@@ -1,24 +1,23 @@
-import { mergeMeta } from '@web-widget/react';
-import type { Manifest, StartOptions } from '@web-widget/web-router';
+import { mergeMeta } from '@web-widget/helpers';
 import WebRouter from '@web-widget/web-router';
+import { meta, manifest } from '@placeholder';
 
-export default (manifest: Manifest, options: StartOptions) => {
-  return WebRouter.fromManifest(manifest, {
-    ...options,
-    defaultMeta: mergeMeta(
+export default WebRouter.fromManifest(manifest, {
+  defaultMeta: mergeMeta(meta, {
+    lang: 'en',
+    meta: [
       {
-        lang: 'en',
-        meta: [
-          {
-            charset: 'utf-8',
-          },
-          {
-            name: 'viewport',
-            content: 'width=device-width, initial-scale=1.0',
-          },
-        ],
+        charset: 'utf-8',
       },
-      options.defaultMeta || {}
-    ),
-  });
-};
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1.0',
+      },
+    ],
+  }),
+  defaultRenderOptions: {
+    react: {
+      awaitAllReady: true,
+    },
+  },
+});
